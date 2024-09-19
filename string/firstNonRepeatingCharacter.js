788566d0da8deb8d7962b31b6f548c39d8421834
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-19 19:35:26 +0600

    feat: add firstNonRepeatingCharacter solution - [string, loop, if-else]

---
+++ string/firstNonRepeatingCharacter.js
@@ -0,0 +1,24 @@
+// Find the First Non-Repeating Character
+// Write a function to find the first character in a string that does not repeat.
+
+function firstNonRepeatingCharacter(str) {
+  let nonRepeatedArray = [];
+  const characters = str.split("");
+
+  for (let i = 0; i < characters.length; i++) {
+    const char = characters[i];
+
+    if (!nonRepeatedArray.includes(char)) {
+      nonRepeatedArray.push(char);
+    } else {
+      const index = nonRepeatedArray.indexOf(char);
+      nonRepeatedArray.splice(index, 1);
+    }
+  }
+
+  return nonRepeatedArray?.length > 0 ? nonRepeatedArray[0] : null;
+}
+
+// Test cases
+console.log(firstNonRepeatingCharacter("aabccde")); // "b"
+console.log(firstNonRepeatingCharacter("aabb")); // null (no non-repeating character)
