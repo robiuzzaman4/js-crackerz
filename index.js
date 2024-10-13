7a750cc04ab12d0402467eee4b571d3e5d604abd
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-13 21:41:24 +0600

    feat: practice capitalizeWords

--- index.js
+++ index.js
@@ -20,5 +20,31 @@ function firstNonRepeatingCharacter(string) {
 }
 
 // Test cases
-console.log(firstNonRepeatingCharacter("aabccde")); // "b"
-console.log(firstNonRepeatingCharacter("aabb")); // null (no non-repeating character)
+// console.log(firstNonRepeatingCharacter("aabccde")); // "b"
+// console.log(firstNonRepeatingCharacter("aabb")); // null (no non-repeating character)
+
+// ========== 02 ==========
+/**
+ * 3. Capitalize the First Character of Each Word
+ * Description: Write a function that capitalizes the first character of each word in a string.
+ * Example: capitalizeWords("hello world"); // Output: "Hello World"
+ *
+ * Requirements:
+ * 1. The function should handle multiple spaces between words.
+ * 2. The function should handle empty strings.
+ */
+function capitalizeWords(string) {
+  if (string.length === 0) {
+    return "enter a valid string";
+  }
+
+  const trimed = string.replace(/\s+/g, " ");
+  const splited = trimed.split(" ");
+
+  const capitalized = splited.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
+  return capitalized.join(" ");
+}
+
+// console.log(capitalizeWords("ruhan    zaman"));
