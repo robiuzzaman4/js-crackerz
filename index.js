bc8c287bb3a6fa096ee66db94fc4b6dfcf8f2508
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-14 12:18:49 +0600

    feat: practice findSecondLargest

--- index.js
+++ index.js
@@ -76,3 +76,30 @@ function countCharacterOccurrences(str, char) {
 // Test cases
 // console.log(countCharacterOccurrences("hello", "l")); // 2
 // console.log(countCharacterOccurrences("mississippi", "s")); // 4
+
+// ========================
+// ========== 04 ==========
+// ========================
+// Find the second largest.
+// You will be given an array as input containing 5 integer numbers. Your task is very simple. You have to find out the second largest element from the numbers.
+
+// Sample Input: [10,33,5,99,6]
+
+// Sample output: 33
+
+function findSecondLargest(numbers) {
+  let firstLargest = 0;
+  let secondLargest = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    if (num > firstLargest) {
+      secondLargest = firstLargest;
+      firstLargest = num;
+    } else if (num > secondLargest && num !== firstLargest) {
+      secondLargest = num;
+    }
+  }
+  return secondLargest;
+}
+// console.log(findSecondLargest([10, 33, 5, 99, 6]));
